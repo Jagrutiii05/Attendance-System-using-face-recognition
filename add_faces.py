b2bbcd323d4339e14058cb3e5337c5ce6df84eac
jagrutiii05 <202151067@iiitvadodara.ac.in>
2023-11-25 01:35:05 +0530

    Frame counter added for samples of a face

--- add_faces.py
+++ add_faces.py
@@ -6,6 +6,10 @@ video = cv2.VideoCapture(0)
 # detect faces using haar cascade classifier
 face_detect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
+faces_data = []
+
+i=0
+
 while True:
     # video.read() method gives two values: 
     # 1. ret: boolean value to tell if the camera is ok
@@ -23,6 +27,13 @@ while True:
 
     # Take coordinates x, y and w, h for width and height
     for (x, y, w, h) in faces:
+        # crop the image and resize every image to same dimension. frame[y:y+h, x:x+w, :] --> crop
+        crop_resized_img = cv2.resize(frame[y:y+h, x:x+w, :], (50,50))
+        if len(faces_data) <= 100 and i%10 == 0:
+            faces_data.append(crop_resized_img)
+        i += 1
+        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_ITALIC, 1, (143,143,255), 2)
+        # draw rectangle around the face
         # image, point1, point2, color of the frame, thickness 
         cv2.rectangle(frame, (x,y), (x+w, y+h), (143,143,255), 2)
 
@@ -31,7 +42,7 @@ while True:
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     # if 'q' is pressed, then break the loop
-    if key == ord('q'):
+    if key == ord('q') or key == ord('Q') or len(faces_data) == 100:
         break
 video.release()
 cv2.destroyAllWindows()
